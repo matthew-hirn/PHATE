5641b368f8eee8c9ea9e145d3fe85afc8147fa68
kmoon3 <kevimoon@gmail.com>
2017-03-24 11:50:06 -0400

    Update svdpca.m

--- svdpca.m
+++ svdpca.m
@@ -10,9 +10,13 @@ switch method
     case 'svd'
         disp 'PCA using SVD'
         [U,~,~] = svds(X', k);
+        Y = X * U;
     case 'random'
         disp 'PCA using random SVD'
         [U,~,~] = randPCA(X', k);
+        Y = X * U;
+    case 'none'
+        disp 'No PCA performed'
+        Y = X;
 end
 
-Y = X * U;
